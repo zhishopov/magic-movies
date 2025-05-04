5cf21de7225bb4709f8b629d63cee0f7204b15a1
Zhivko <zhishopov@gmail.com>
2025-05-04 12:46:48 +0100

    Project setup

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import handlebars from "express-handlebars";
+
+const app = express();
+
+app.engine("hbs", handlebars.engine({
+    extname: "hbs"
+}))
+
+app.set("view engine", "hbs")
+
+app.get('/', (req, res) => {
+    res.render("home");
+})
+
+app.listen(5000, () => console.log("Server is listening on http://localhost:5000..."))
